fd52424ff105a9366cd343daca370bd0414dd7cc
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-03-27 13:26:59 +0100

    source-code: format & LF line-endings

--- routes.ts
+++ routes.ts
@@ -5,7 +5,7 @@ let router: Router = express.Router()
 // curl http://localhost:8080/?name1=12345
 router.get("/", (request, response) => {
     // HTTP request & HTTP response
-    response.json( request.query )
+    response.json(request.query)
 })
 
 export default router
