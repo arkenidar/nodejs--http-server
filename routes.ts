d95d852f614dc97dc8ac9f6df999860489f892c9
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-03-27 18:05:30 +0100

    router.get("/sql" router.get("/sql2"

--- routes.ts
+++ routes.ts
@@ -1,6 +1,8 @@
-import express, { Router } from "express"
+import { Router } from 'express'
+let router: Router = Router()
+export default router
 
-let router: Router = express.Router()
+import { Connection } from "mysql2/promise"
 
 // curl http://localhost:8080/?name1=12345
 router.get("/", (request, response) => {
@@ -8,4 +10,29 @@ router.get("/", (request, response) => {
     response.json(request.query)
 })
 
-export default router
+// curl http://localhost:8080/sql
+router.get("/sql", async (request, response) => {
+    const connection: Connection = request.app.get("connection")
+
+    const sql = 'select ?+? as result'
+    const params = [11, 33]
+    const [rows] = await connection.execute<any[]>(sql, params)
+    console.log("result is", rows[0].result)
+
+    response.json(rows)
+})
+
+// curl http://localhost:8080/sql2?x=5
+router.get("/sql2", async (request, response) => {
+    const x: string = <string>request.query.x
+    const number: number = parseFloat(x || "0")
+
+    const connection: Connection = request.app.get("connection")
+
+    const sql = 'select ?*2 as result'
+    const params = [number]
+    const [rows] = await connection.execute<any[]>(sql, params)
+    console.log("result is", rows[0].result)
+
+    response.json(rows)
+})
