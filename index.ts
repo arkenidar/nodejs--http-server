100b5ee35d10250497eba1596ab24b5517f91327
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2024-03-27 18:04:13 +0100

    import { Connection, createConnection } from "mysql2/promise"

--- index.ts
+++ index.ts
@@ -6,6 +6,23 @@ import express from 'express'
 
 let server = express()
 
+// https://www.npmjs.com/package/mysql2
+import { Connection, createConnection } from "mysql2/promise"
+
+const connection: Connection = await createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "mydb",
+})
+
+server.set("connection", connection)
+
+const sql = 'select ?+? as result'
+const params = [11, 33]
+const [rows] = await connection.execute<any[]>(sql, params)
+console.log("result is", rows[0].result)
+
 import router from './routes.js'
 server.use("/", router)
 
